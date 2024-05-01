6bfe29451c3fc14d0288b0972c0048418fdce70f
takapdayon <business.takap@gmail.com>
2024-05-02 00:19:05 +0900

    🚧 404ページの追加なのだ
    ピクセス文字っぽいページにしたのだ

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { DotGothic16 } from 'next/font/google';
+import Link from 'next/link';
+const dotGothic16 = DotGothic16({ subsets: ['latin'], weight: '400' });
+
+export default function NotFound() {
+  return (
+    <div className={`${dotGothic16.className} flex flex-col items-center`}>
+      <h2 className="my-20 text-5xl text-red-500">404 Not Found</h2>
+      <p className="mb-2">その仮想世界はまだないみたい...</p>
+      <Link className="text-blue-400" href="/">
+        現実世界に戻る
+      </Link>
+    </div>
+  );
+}
