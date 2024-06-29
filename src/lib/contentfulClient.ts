136d2d85f7fd37fe1c6c78eed2d71fac53b11c0e
takapdayon <business.takap@gmail.com>
2024-06-29 16:22:36 +0900

    🔧 Preview用envに伴う変更

--- src/lib/contentfulClient.ts
+++ src/lib/contentfulClient.ts
@@ -3,6 +3,9 @@ import { createClient } from 'contentful';
 export const createCfClient = (preview: boolean = false) =>
   createClient({
     space: process.env.CONTENTFUL_SPACE_ID ?? '',
-    accessToken: preview ? process.env.CONTENTFUL_PREVIEW_TOKEN ?? '' : process.env.CONTENTFUL_DELIVERY_TOKEN ?? '',
-    host: preview ? 'preview.contentful.com' : undefined,
+    accessToken:
+      preview || process.env.IS_PREVIEW
+        ? process.env.CONTENTFUL_PREVIEW_TOKEN ?? ''
+        : process.env.CONTENTFUL_DELIVERY_TOKEN ?? '',
+    host: preview || process.env.IS_PREVIEW ? 'preview.contentful.com' : undefined,
   });
