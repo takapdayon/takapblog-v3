08054221715c5bd0829048e907db8fafb7e458ca
takapdayon <business.takap@gmail.com>
2025-01-26 16:06:42 +0900

    🐛 Fixed draft mode settings

--- src/lib/contentfulClient.ts
+++ src/lib/contentfulClient.ts
@@ -1,11 +1,11 @@
 import { createClient } from 'contentful';
 
-export const createCfClient = (preview: boolean = false) =>
-  createClient({
+export const createCfClient = (preview: boolean = false) => {
+  const isPreview = process.env.IS_PREVIEW?.toLowerCase() === 'true';
+  return createClient({
     space: process.env.CONTENTFUL_SPACE_ID ?? '',
     accessToken:
-      preview || process.env.IS_PREVIEW
-        ? process.env.CONTENTFUL_PREVIEW_TOKEN ?? ''
-        : process.env.CONTENTFUL_DELIVERY_TOKEN ?? '',
-    host: preview || process.env.IS_PREVIEW ? 'preview.contentful.com' : undefined,
+      preview || isPreview ? process.env.CONTENTFUL_PREVIEW_TOKEN ?? '' : process.env.CONTENTFUL_DELIVERY_TOKEN ?? '',
+    host: preview || isPreview ? 'preview.contentful.com' : undefined,
   });
+};
