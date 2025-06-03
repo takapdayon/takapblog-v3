34758d281f8260c2dcef9729d3afe5611881df6c
takapdayon <business.takap@gmail.com>
2025-06-03 20:29:38 +0900

    🧑‍💻 ホットリロードされるようにmjsを修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: config => {
+    config.watchOptions = {
+      ignored: /node_modules/,
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
