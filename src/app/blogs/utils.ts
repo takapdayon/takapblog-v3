08620053dbedb2251defcad627002f38806921fa
takapdayon <business.takap@gmail.com>
2025-06-03 22:11:52 +0900

    ♻️ Magic numberをなくした

--- src/app/blogs/utils.ts
+++ src/app/blogs/utils.ts
@@ -3,8 +3,9 @@ import type { TypeBlogSkeleton } from '@/types/contentful';
 
 const CONTENT_TYPE = 'blog';
 const ORDER_PUBLISHED_DATE = '-fields.publishedDate';
+const PER_PAGE = 9;
 
-export const getPosts = async (isDraftModeEnabled: boolean = false, page: number = 1, limit: number = 9) => {
+export const getPosts = async (isDraftModeEnabled: boolean = false, page: number = 1, limit: number = PER_PAGE) => {
   const skip = (page - 1) * limit;
   const posts = await createCfClient(isDraftModeEnabled).getEntries<TypeBlogSkeleton>({
     content_type: CONTENT_TYPE,
